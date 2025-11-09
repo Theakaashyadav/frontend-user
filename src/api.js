339e5d4b9d76dc4f6f8ce59ce080ded7dffacdb4
Akash yadav <theakaashyadav@gmail.com>
2025-11-10 01:07:57 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,10 +2,10 @@ let API_BASE = "";
 
 if (window.location.hostname.includes("localhost")) {
   // Local frontend talking to Render backend
-  API_BASE = "https://akash-pp66.onrender.com/api";
+  API_BASE = "https://akash-itf3.onrender.com/api";
 } else {
   // Production frontend on Render
-  API_BASE = "https://akash-pp66.onrender.com/api";
+  API_BASE = "https://akash-itf3.onrender.com/api";
 }
 
 export { API_BASE };
