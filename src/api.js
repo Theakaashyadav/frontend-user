549f93781fa479aa07c2a5b311f7c23a51fd8657
Akash yadav <theakaashyadav@gmail.com>
2025-11-16 20:59:55 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,10 +2,10 @@ let API_BASE = "";
 
 // Local frontend
 if (window.location.hostname.includes("localhost")) {
-  API_BASE = "http://52.66.236.122:4000/api";
+  API_BASE = "https://52.66.236.122:4000/api";
 } else {
   // Production frontend on Render
-  API_BASE = "http://52.66.236.122:4000/api";
+  API_BASE = "https://52.66.236.122:4000/api";
 }
 
 export { API_BASE };
