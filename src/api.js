8dc51b69111a76c53dde16eee5802b2b39748e45
Akash yadav <theakaashyadav@gmail.com>
2025-11-17 18:06:05 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,10 +2,10 @@ let API_BASE = "";
 
 // Local frontend (localhost)
 if (window.location.hostname.includes("localhost")) {
-  API_BASE = "https://akash-1-4g8j.onrender.com/api";
+  API_BASE = "https://laughing-happiness-5g76j69rqq44f7g6p-4000.app.github.dev/api";
 } else {
   // Production frontend (Render)
-  API_BASE = "https://akash-1-4g8j.onrender.com/api";
+  API_BASE = "https://laughing-happiness-5g76j69rqq44f7g6p-4000.app.github.dev/api";
 }
 
 export { API_BASE };
