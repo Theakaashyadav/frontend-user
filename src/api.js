71d751e6ec1578a878cdb0385e8b3e11e890085a
Akash yadav <theakaashyadav@gmail.com>
2025-11-26 11:16:08 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,10 +2,10 @@ let API_BASE = "";
 
 // Local frontend (localhost)
 if (window.location.hostname.includes("localhost")) {
-  API_BASE = "https://r55nc746-4000.inc1.devtunnels.ms//api";
+  API_BASE = "https://akash-1-4g8j.onrender.com/api";
 } else {
   // Production frontend (Render)
-  API_BASE = "https://r55nc746-4000.inc1.devtunnels.ms//api";
+  API_BASE = "https://akash-1-4g8j.onrender.com/api";
 }
 
 export { API_BASE };
