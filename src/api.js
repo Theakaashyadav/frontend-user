cf8a46231a80657fe64421d74e9d918e039c6266
Akash yadav <theakaashyadav@gmail.com>
2025-11-28 09:57:28 +0530

    Set API_BASE to production URL

--- src/api.js
+++ src/api.js
@@ -1,16 +1,5 @@
 // src/api.js
 
-let API_BASE = "";
-
-if (window.location.hostname.includes("devtunnels.ms")) {
-  API_BASE = "https://akash-1-4g8j.onrender.com/api";
-} else if (window.location.hostname.includes("localhost")) {
-  API_BASE = "https://akash-1-4g8j.onrender.com/api";
-} else {
-  // Production backend on Render
-  API_BASE = "https://akash-1-4g8j.onrender.com/api";
-}
+let API_BASE = "https://akash-1-4g8j.onrender.com/api";
 
 export { API_BASE };
-
-
